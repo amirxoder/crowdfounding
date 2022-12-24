a595f37e11a3681be94ddf5d4671f3fa21a01613
Amir Codder <amirxoder@gmail.com>
2022-12-24 23:17:58 +0330

    navbar completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { CustomButton } from "./";
+import { logo, menu, search, thirdweb } from "../assets";
+import { navlinks } from "../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+  const address = "0xasd12..";
+
+  return (
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
+        <input
+          type={"text"}
+          placeholder="Search for Campaigns"
+          className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none"
+        />
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+          <img
+            src={search}
+            alt={search}
+            className="w-[15px] h-[15px] object-contain "
+          />
+        </div>
+      </div>
+
+      <div className={"sm:flex hidden flex-row justify-end items-center gap-4"}>
+        <CustomButton
+          btnType="button"
+          title={address ? "Create a campaign" : "Connect"}
+          styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+          handleClick={() => {
+            if (address) navigate("create-campign");
+            else "connect()";
+          }}
+        />
+
+        <Link to={"/profile"}>
+          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex items-center justify-center cursor-pointer">
+            <img
+              src={thirdweb}
+              alt="user"
+              className="w-[60%] h-[60%] object-contain"
+            />
+          </div>
+        </Link>
+      </div>
+
+      {/* small screen navigation */}
+
+      <div className="sm:hidden flex justify-between items-center relative">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex items-center justify-center cursor-pointer">
+          <img
+            src={thirdweb}
+            alt="user"
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+
+        <img
+          src={menu}
+          alt="menu"
+          className="w-[34px] h-[34px] object-contain cursor-pointer"
+          onClick={() => setToggleDrawer((prev) => !prev)}
+        />
+
+        <div
+          className={`absolute top-[60px] right-[0] left-[0] bg-[#1c1c24] z-10 shadow-secondary
+        py-4 ${
+          !toggleDrawer ? "-translate-y-[100vh]" : "translate-y-0"
+        } transition-transform duration-700`}
+        >
+          <ul className="mb-4">
+            {navlinks.map((item, i) => (
+              <li
+                key={i}
+                className={`flex p-4 ${
+                  isActive === item.name && "bg-[#3a3a43]"
+                }`}
+                onClick={() => {
+                  setIsActive(item.name);
+                  setToggleDrawer(false);
+                  navigate(item.link);
+                }}
+              >
+                <img
+                  src={item.imgUrl}
+                  alt={item.name}
+                  className={`w-[24px] h-[24px] object-contain 
+                ${isActive === item.name ? "grayscale-0" : "grayscale"}`}
+                />
+                <p
+                  className={`ml-[20px] font-epilogue font-semibold text-[14px] ${
+                    isActive === item.name
+                      ? "text-[#1dc071]"
+                      : " text-[#808191]"
+                  }`}
+                >
+                  {item.name}
+                </p>
+              </li>
+            ))}
+          </ul>
+
+          <div
+            className="flex w-full justify-center"
+          >
+            <CustomButton
+              btnType="button"
+              title={address ? "Create a campaign" : "Connect"}
+              styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+              handleClick={() => {
+                if (address) navigate("create-campign");
+                else "connect()";
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
